5b7075b80d2a222d7b2c3565bb209939ccd299dc
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2015-10-20 22:27:58 +0300

    добавил первый боксплот

--- task1/boxplot.R
+++ task1/boxplot.R
@@ -1,6 +1,10 @@
+library(tikzDevice)
+
 file_sample <- 'data/n.txt'
 
 sample <- read.table(file_sample)
 sample <- as.vector(t(sample))
 
+tikz('tex/boxplot.tex',width=3.5,height=3.5)
 boxplot(sample)
+dev.off()
