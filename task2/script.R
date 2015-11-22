4e96dbd555e543b6362e7e8010a399a5caf4795c
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2015-11-22 12:37:42 +0300

    сделал рефакторинг скрипта

--- task2/script.R
+++ task2/script.R
@@ -1,8 +1,10 @@
+# ядерная функция
 K <- function(u){
   result <- exp(-u^2/2) / sqrt(2*pi)
   return(result)
 }
 
+# вычисление ядерной оценки плотности по выборке в опорных точках
 f <- function(dataset, h, x_range_vector=seq(-4, 4, length=100)){
   result <- double(length(x_range_vector))
   for( j in x_range_vector){
@@ -16,25 +18,30 @@ f <- function(dataset, h, x_range_vector=seq(-4, 4, length=100)){
   return(result)
 }
 
+# отрисовка графика плотности нормального распределения и ядерной оценки
+plot_kde <- function(x_range, h, color, label){
+  hx <- dnorm(x_range) # нормальное распределение
+  plot(x_range, hx, xlab="x", ylab="Density", lwd=2, type="l")
+  sx <- f(x, h, x_range)
+  lines(x_range, sx, lwd=2, col=color)
+  legend("topright", legend=c("Normal", label), lwd=2, col=c("black", color))
+}
+
+# считали выборку и преобразовали в вектор
 sample <- read.table("sample_N_0_1.txt")
 sample <- as.vector(t(sample))
 
+# вычисляем h silverman'а
 x <- sample
 x_mean <- mean(x)
 n <- length(x)
 S <- sqrt(sum((x - x_mean)^2)/length(x))
 h <- S / n^(1/5)
 
+# создаём вектор опорных точек для построения нормального распределения и ядерных оценок
 x_range <- seq(-4, 4, length=200)
 
-hx <- dnorm(x_range)
-plot(x_range, hx, type="l")
-
-sx1 <- f(x, h, x_range)
-lines(x_range, sx1, col="red")
-
-sx2 <- f(x, h*2, x_range)
-lines(x_range, sx2, col="blue")
-
-sx3 <- f(x, h/2, x_range)
-lines(x_range, sx3, col="green")
+# построение графиков
+plot_kde(x_range, h, "red", "h = h_silverman")
+plot_kde(x_range, 2*h, "blue", "h = 2 * h_silverman")
+plot_kde(x_range, h/2, "green", "h = h_silverman / 2")
