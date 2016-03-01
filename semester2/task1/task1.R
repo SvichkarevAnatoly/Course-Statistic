9105ddad6f452f1d009986278bd9b97d767b14d1
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-03-02 01:01:44 +0300

    реализовал t-test и вычислил для всех пар

--- semester2/task1/task1.R
+++ semester2/task1/task1.R
@@ -1,6 +1,10 @@
 # Чтобы генерировались одинаковые выборки
 set.seed(17031993)
 
+# =======================================================
+# Инициализация выборок и вычисление их характеристик
+# =======================================================
+
 # Размеры выборок и смещение матожидания
 sampleVolumes <- c(10, 20, 100)
 meanShifts <- c(0.1, 0.5, 1)
@@ -12,7 +16,6 @@ standartSample <- vector("list", volumesNumber)
 for(i in 1:length(sampleVolumes)){
     standartSample[[i]] <- rnorm(sampleVolumes[i], 0, 1)
 }
-s1 <- standartSample[[1]]
 
 samples <- matrix(rep(list(), volumesNumber*meansNumber),
                  nrow = meansNumber, ncol = volumesNumber)
@@ -28,9 +31,11 @@ for(i in 1:volumesNumber){ # по строкам в матрице
         samplesSDs[[i, j]] <- sd(samples[[i, j]])
     }
 }
-s2 <- samples[[1,2]]
 
-t_test <- function(s1, s2){
+# =======================================================
+# Критерий t-Стьюдента
+# =======================================================
+TTest <- function(s1, s2){
     s1Mean <- mean(s1)
     s2Mean <- mean(s2)
     s1SD <- sd(s1)
@@ -38,8 +43,37 @@ t_test <- function(s1, s2){
     n1 <- length(s1)
     n2 <- length(s2)
     
-    t <- (s1Mean - s2Mean) / sqrt((s1SD^2)/n1 + (s2SD^2)/n2)
+    t <- (s1Mean - s2Mean) /
+        sqrt((s1SD^2)/n1 + (s2SD^2)/n2)
     return(t)
 }
 
-t_test(s1,s2)
+# qt(.95, 18)
+
+
+TTestRunner <- function(){
+    cat("t-test:\n")
+    for(i in 1:volumesNumber){ # по строкам в матрице
+        for(j in 1:meansNumber){ # по столбцам в матрице
+            s1 <- samples[[i, j]] # сдвинутая выборка
+            s2 <- standartSample[[i]] # оригинальная выборка
+            t <- TTest(s1, s2)
+            cat("N = ", sampleVolumes[i],
+                "\tshift = ", meanShifts[j],
+                "\tt = ", t, "\n")
+        }
+    }
+}
+TTestRunner()
+
+# Для проверки правильности расчётов t
+# t_test(s2,s1)
+# t.test(s2,s1, var.equal=TRUE, paired=FALSE)
+
+# =======================================================
+# Критерий Колмагорова-Смирнова
+# =======================================================
+
+# =======================================================
+# Критерий Вилкоксона
+# =======================================================
