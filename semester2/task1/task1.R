a62b8f748335ccc3fa3a97f6d483598ea3886590
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-03-01 22:42:50 +0300

    реализовал вычисление t-test

--- semester2/task1/task1.R
+++ semester2/task1/task1.R
@@ -12,22 +12,34 @@ standartSample <- vector("list", volumesNumber)
 for(i in 1:length(sampleVolumes)){
     standartSample[[i]] <- rnorm(sampleVolumes[i], 0, 1)
 }
+s1 <- standartSample[[1]]
 
-standartSample[[1]]
-mean(standartSample[[1]])
-sd(standartSample[[1]])
+samples <- matrix(rep(list(), volumesNumber*meansNumber),
+                 nrow = meansNumber, ncol = volumesNumber)
+samplesMeans <- matrix(volumesNumber*meansNumber,
+                 nrow = meansNumber, ncol = volumesNumber)
+samplesSDs <- matrix(volumesNumber*meansNumber,
+                 nrow = meansNumber, ncol = volumesNumber)
 
-a1 <- rnorm(10, 0.1, 1)
-a2 <- rnorm(10, 0.5, 1)
-a2
-mean(a2)
-sd(a2)
+for(i in 1:volumesNumber){ # по строкам в матрице
+    for(j in 1:meansNumber){ # по столбцам в матрице
+        samples[[i, j]] <- rnorm(sampleVolumes[i], meanShifts[j], 1)
+        samplesMeans[[i, j]] <- mean(samples[[i, j]])
+        samplesSDs[[i, j]] <- sd(samples[[i, j]])
+    }
+}
+s2 <- samples[[1,2]]
 
-#samples <- matrix(rep(list(), volumesNumber*meansNumber),
-#                  nrow = meansNumber, ncol = volumesNumber)
-#for(i in 1:volumesNumber){
-#    for(j in 1:meansNumber){
-        # samples[i, j] <- rnorm(sampleVolumes[i], meanShifts[j], 1)
-#        samples[i, j] <- rnorm(3, 0, 1)
-#    }
-#}
+t_test <- function(s1, s2){
+    s1Mean <- mean(s1)
+    s2Mean <- mean(s2)
+    s1SD <- sd(s1)
+    s2SD <- sd(s2)
+    n1 <- length(s1)
+    n2 <- length(s2)
+    
+    t <- (s1Mean - s2Mean) / sqrt((s1SD^2)/n1 + (s2SD^2)/n2)
+    return(t)
+}
+
+t_test(s1,s2)
